b9e8716e0178f4d920c6a2d96595606171a13950
Lara Glenn <146271102+lara-glenn@users.noreply.github.com>
2023-11-23 19:04:57 +0000

    Created menu, prints number of records and imports correct file

--- task2/FitnessDataStruct.h
+++ task2/FitnessDataStruct.h
@@ -4,14 +4,30 @@
 
 #include <stdio.h>
 
+
 // Define an appropriate struct
 typedef struct {
 	char date[11];
 	char time[6];
-	int steps;
+	char steps[1000];
 } FITNESS_DATA;
+
 
 // Helper function prototypes
 void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
 
 #endif // FITNESS_DATA_STRUCT_H
+FILE *open_file(char *filename, char *mode)
+{
+	FILE *input = fopen(filename, mode);
+	if (!input)
+	{
+		printf("Error: could not open file\n");
+		return NULL;
+	}
+
+	return input;
+
+}
+
+
--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -6,6 +6,9 @@
 // Struct moved to header file
 
 // Define any additional variables here
+    int counter = 0;
+    int i;  
+    int integer_steps;
 // Global variables for filename and FITNESS_DATA array
 
 
@@ -37,11 +40,108 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
                     }
 
+FILE* importfile()
+{
+
+	int buffer_size = 1000;
+    char line[buffer_size];
+    char filename[buffer_size];
+	printf("Input filename:");
+
+    fgets(line, buffer_size, stdin);
+
+    sscanf(line, "%s", filename);
+
+    FILE *input = open_file(filename, "r");
+
+    printf("File opened\n");
+
+    return input;
+
+    //fclose(input);
+}
 
 
+void totalrecords(FILE *filerecords)
+{
+    rewind(filerecords);
+	FITNESS_DATA data[1000];
+	int counter = 0;
+
+
+	char date[1000];
+    char time[500];
+    char steps[500];
+
+	int buffer_size = 1000;
+    char line[buffer_size];
+    char filename[buffer_size];
+
+	while (fgets(line, buffer_size, filerecords)){
+        tokeniseRecord(line, ",", date, time, steps);
+
+        strcpy(data[counter].date, date);
+        strcpy(data[counter].time, time);
+        strcpy(data[counter].steps, steps);
+
+        counter++;
+    }
+
+    printf("Number of records in file: %d\n", counter); 
+}
 
 // Complete the main function
 int main() {
+    FILE *input = NULL;
+
+    FITNESS_DATA data[1000];
+    int buffer_size = 1000;
+    char line[buffer_size];
+    char filename[buffer_size];
+    char chosen_option; 
+
+    while(1)
+    {
+        printf("Menu Options\n");
+        printf("A: Specify the filename to be imported\n");
+        printf("B: Display the total number of records in the file\n");
+        printf("C: Find the date and time of the timeslot with the fewest steps\n");
+        printf("D: Find the date and time of the timeslot with the largest number of steps\n");
+        printf("E: Find the mean step count of all the records in the file\n");
+        printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+        printf("Q: Quit\n");
+
+    
+
+    chosen_option = getchar();
+
+    while (getchar() != '\n');
+
+    switch(chosen_option){
+        case 'A':
+        case 'a':
+            input = importfile(); 
+            break;
+
+        case 'B':
+        case 'b':
+            totalrecords(input);
+            break;
+        case 'Q':
+        case 'q':
+            return 0;
+            break;
+
+    }
+
+        
+
+            
+    }
+
+    
+    
+    return 0;
    
 }
 
