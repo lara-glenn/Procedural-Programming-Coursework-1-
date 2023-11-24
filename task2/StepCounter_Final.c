8ac21897493242193300fb5b668bc44a574b349d
Lara Glenn <146271102+lara-glenn@users.noreply.github.com>
2023-11-24 17:20:29 +0000

    Created function to calculate the mean and have rounded to nearest value and printed as an integer

--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "FitnessDataStruct.h"
+
 
 // Struct moved to header file
 
+
 // Define any additional variables here
-    int counter = 0;
-    int i;  
-    int integer_steps;
+
+int integer_steps;
 // Global variables for filename and FITNESS_DATA array
+FITNESS_DATA data[1000];
+int counter = 0;
+
 
 
 // This is your helper function. Do not change it in any way.
@@ -19,86 +24,150 @@ void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
-    
+   
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
     if (token != NULL) {        strcpy(date, token);
     }
-    
+   
     token = strtok(NULL, delimiter);
     if (token != NULL) {
         strcpy(time, token);
     }
-    
+   
     token = strtok(NULL, delimiter);
     if (token != NULL) {
         strcpy(steps, token);
     }
-    
+   
     // Free the duplicated string
     free(inputCopy);
 
+
                     }
+
 
 FILE* importfile()
 {
 
-	int buffer_size = 1000;
+
+    int buffer_size = 1000;
     char line[buffer_size];
     char filename[buffer_size];
-	printf("Input filename:");
+    printf("Input filename:");
+
 
     fgets(line, buffer_size, stdin);
 
+
     sscanf(line, "%s", filename);
+
 
     FILE *input = open_file(filename, "r");
 
+
     printf("File opened\n");
 
+
     return input;
+
 
     //fclose(input);
 }
 
 
+
+
 void totalrecords(FILE *filerecords)
 {
     rewind(filerecords);
-	FITNESS_DATA data[1000];
-	int counter = 0;
 
 
-	char date[1000];
+    char date[1000];
     char time[500];
     char steps[500];
 
-	int buffer_size = 1000;
+
+    int buffer_size = 1000;
     char line[buffer_size];
     char filename[buffer_size];
 
-	while (fgets(line, buffer_size, filerecords)){
+
+    while (fgets(line, buffer_size, filerecords)){
         tokeniseRecord(line, ",", date, time, steps);
+
 
         strcpy(data[counter].date, date);
         strcpy(data[counter].time, time);
         strcpy(data[counter].steps, steps);
 
+
         counter++;
     }
 
-    printf("Number of records in file: %d\n", counter); 
+
+    printf("Number of records in file: %d\n", counter);
 }
 
+
+float calculatemean(FILE *filemean)
+{
+    rewind(filemean);
+
+    FITNESS_DATA data[1000];
+    char date[1000];
+    char time[500];
+    char steps[500];
+    int num = 0;
+    int i=0;
+    int buffer_size = 1000;
+    char line[buffer_size];
+    char filename[buffer_size];
+
+
+    while (fgets(line, buffer_size, filemean)){
+        tokeniseRecord(line, ",", date, time, steps);
+
+
+        int numSteps = atoi(steps);
+
+
+        num += numSteps;
+
+
+        i++;
+    }
+
+    float mean =(float) num/i;
+    int result = mean + 0.5;
+
+
+    //printf("Mean step count: %.2f\n", mean);
+    printf("Mean step count: %d\n", result);
+    return result;
+    return mean;
+
+
+
+}
+
+
+void largestSteps(FILE *filelarge){
+
+
+   
+}
 // Complete the main function
 int main() {
     FILE *input = NULL;
+
 
     FITNESS_DATA data[1000];
     int buffer_size = 1000;
     char line[buffer_size];
     char filename[buffer_size];
-    char chosen_option; 
+    char chosen_option;
+
 
     while(1)
     {
@@ -111,38 +180,59 @@ int main() {
         printf("F: Find the longest continuous period where the step count is above 500 steps\n");
         printf("Q: Quit\n");
 
-    
+
+   
+
 
     chosen_option = getchar();
 
+
     while (getchar() != '\n');
+
 
     switch(chosen_option){
         case 'A':
         case 'a':
-            input = importfile(); 
+            input = importfile();
             break;
+
 
         case 'B':
         case 'b':
             totalrecords(input);
+            break;
+
+
+        case 'E':
+        case 'e':
+            calculatemean(input);
+
             break;
         case 'Q':
         case 'q':
             return 0;
             break;
 
+
     }
 
-        
 
-            
+       
+
+
+           
     }
 
-    
-    
+
+   
+   
     return 0;
    
 }
+
+
+
+
+
 
 
